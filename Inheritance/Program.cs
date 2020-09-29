fc591e329142c5e6f47ec71535f756442eb731d6
Ryan Hagood <ryanh147@gmail.com>
2020-09-28 20:54:37 -0500

    Committing Completed Inheritance Exercise

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inheritance
 {
@@ -8,28 +9,54 @@ namespace Inheritance
         {
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
+            // DONE - Create a class Animal
             // give this class 4 members that all Animals have in common
 
 
-            // Create a class Bird
+            // DONE - Create a class Bird
             // give this class 4 members that are specific to Bird
             // Set this class to inherit from your Animal Class
 
-            // Create a class Reptile
+            // DONE - Create a class Reptile
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
 
 
 
 
-            /*Create an object of your Bird class
+            /*Done - Create an object of your Bird class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
 
-            /*Create an object of your Reptile class
+            var lizard = new Reptile()
+            {
+            IsColdBlooded = true,
+            IsScaly = true,
+            Habitat = "swamp",
+            CanGrowTail = true
+
+            };
+
+            var myAnimals = new Animal[] { myBird, lizard };
+
+            foreach(var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has {animal.LegCount} Legs");
+                Console.WriteLine($"It lives by {animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+          
+
+            /*Done - Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
